78f1187b32d3dbd9b3dac799f75e809a1d94878b
Andrea Aloi <axedre@gmail.com>
2014-04-05 11:14:51 +0200

    Signin template page added, init script modified to remove git folder, npm start script added to package.json

--- init.sh
+++ init.sh
@@ -8,3 +8,4 @@ echo "[WARNING: this can only be done once, so choose wisely]"
 echo -n "Choose a name for your app: ";
 read appName;
 grep -Rl $default . | grep -v ".git" | grep -v "angular.js" | grep -v $0 | xargs sed -i "s/$default/$appName/g";
+rm -rf .git
