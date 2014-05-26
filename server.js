33c402dd6f39b188b882de6f33fa4ff68816d351
Andrea Aloi <axedre@gmail.com>
2014-05-26 22:15:40 +0200

    Fixed path for express' static middleware

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var _ = require("underscore");
 var app = express();
 var server = require("http").createServer(app);
 app.set("port", 4000);
-app.use(express.static(__dirname + "/../app"));
+app.use(express.static(__dirname + "/app"));
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With");
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
